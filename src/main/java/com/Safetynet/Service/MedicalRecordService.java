3b04bca7e3d30080335ea5c9e46e04b1c018acb2
Achille Deribreux <achille@deribreux.be>
2021-11-02 15:47:31 +0100

    MedicalRecordService

--- src/main/java/com/Safetynet/Service/MedicalRecordService.java
+++ src/main/java/com/Safetynet/Service/MedicalRecordService.java
@@ -1,7 +1,28 @@
 package com.Safetynet.Service;
 
+import com.Safetynet.Model.MedicalRecords;
+import com.Safetynet.Repository.MedicalRecordsDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MedicalRecordService {
+public class MedicalRecordService implements IMedicalRecordService{
+
+    @Autowired
+    MedicalRecordsDAO medicalRecordsDAO;
+
+    @Override
+    public MedicalRecords addMedicalRecords(MedicalRecords medicalRecords) {
+        return medicalRecordsDAO.addMedicalRecords(medicalRecords);
+    }
+
+    @Override
+    public MedicalRecords editMedicalRecords(MedicalRecords medicalRecords) {
+        return medicalRecordsDAO.editMedicalRecords(medicalRecords);
+    }
+
+    @Override
+    public void deleteMedicalRecords(MedicalRecords medicalRecords) {
+        medicalRecordsDAO.deleteMedicalRecords(medicalRecords);
+    }
 }
