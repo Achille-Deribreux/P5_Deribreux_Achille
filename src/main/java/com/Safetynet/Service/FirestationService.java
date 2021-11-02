ac41a6cbd63fe5a7a822782b58548ae9da670829
Achille Deribreux <achille@deribreux.be>
2021-11-02 15:41:51 +0100

    FirestationService

--- src/main/java/com/Safetynet/Service/FirestationService.java
+++ src/main/java/com/Safetynet/Service/FirestationService.java
@@ -1,10 +1,28 @@
 package com.Safetynet.Service;
 
 import com.Safetynet.Model.Firestations;
+import com.Safetynet.Repository.FirestationDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FirestationService {
+public class FirestationService implements IFirestationService {
 
+    @Autowired
+    FirestationDAO firestationDAO;
 
+    @Override
+    public Firestations addFirestation(Firestations firestations) {
+        return firestationDAO.addFirestations(firestations);
+    }
+
+    @Override
+    public Firestations editFirestations(Firestations firestations) {
+        return firestationDAO.editFirestations(firestations);
+    }
+
+    @Override
+    public void deleteFirestations(Firestations firestations) {
+        firestationDAO.deleteFirestations(firestations);
+    }
 }
