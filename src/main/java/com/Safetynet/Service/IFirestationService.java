63cdc01a33d853ad9f845001117adc84ff81dcdc
Achille Deribreux <achille@deribreux.be>
2021-11-02 16:01:19 +0100

    FirestationController

--- src/main/java/com/Safetynet/Service/IFirestationService.java
+++ src/main/java/com/Safetynet/Service/IFirestationService.java
@@ -5,6 +5,6 @@ import com.Safetynet.Model.Firestations;
 public interface IFirestationService {
 
     public Firestations addFirestation(Firestations firestations);
-    public Firestations editFirestations(Firestations firestations);
-    public void deleteFirestations(Firestations firestations);
+    public Firestations editFirestation(Firestations firestations);
+    public void deleteFirestation(Firestations firestations);
 }
