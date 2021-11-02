2179ba2a8e0f3cc869b90a921ca796a83db23515
Achille Deribreux <achille@deribreux.be>
2021-11-02 15:56:31 +0100

    PersonController

--- src/main/java/com/Safetynet/Controller/PersonController.java
+++ src/main/java/com/Safetynet/Controller/PersonController.java
@@ -3,9 +3,9 @@ package com.Safetynet.Controller;
 import com.Safetynet.Model.Person;
 import com.Safetynet.Service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -14,4 +14,19 @@ public class PersonController {
     @Autowired
     PersonService personService;
 
+    //TODO: Get some responses & codes
+    @PostMapping(value="/person", consumes = "application/json", produces = "application/json")
+    public void addPerson(@RequestBody Person person) {
+        personService.addPerson(person);
+    }
+
+    @PutMapping(value="/person", consumes = "application/json", produces = "application/json")
+    public void editPerson(@RequestBody Person person) {
+        personService.editPerson(person);
+    }
+
+    @DeleteMapping(value="/person", consumes = "application/json")
+    public void deletePerson(@RequestBody Person person) {
+        personService.deletePerson(person);
+    }
 }
